811e5592f03b9aac0e8e8f0c5fc367d013f27508
eerhs7 <hemashreen70@gmail.com>
2023-01-13 22:42:24 +0530

    dashboard added

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -1,7 +1,51 @@
+import '../styles/AdminDashboard.css'
+
 const AdminDashboard = () => {
+    let content = [
+        {
+            title: "Greenwich Library to Host an Evening of Broadway Cabaret",
+            author: "By Erika Berlin on January 12, 2023",
+            desc: "The Friends of Greenwich Library are pleased to host a special night of cabaret-style performances with How I Got to…",
+            link: "Take a look"
+        },
+        {
+            title: "Signature Series: In the News with Fareed Zakaria",
+            author: "By Erika Berlin on January 11, 2023",
+            desc: "Greenwich Library’s Signature Series is pleased to welcome bestselling author and journalist Fareed Zakaria to the Library’s Berkley Theater on Thursday,…",
+            link: "Take a look"
+        },
+        {
+            title: "At the Library this January …",
+            author: "By Erika Berlin on December 29, 2022",
+            desc: "Greenwich Library has a full schedule of (free!) enrichment events throughout January. Speak with a librarian or check the online…",
+            link: "Take a look"
+        }
+    ]
     return (
         <div className="admin_dash">
-            <h1>Welcome to the library</h1>
+            <div className="img">
+                <p>Welcome to Admin Dashboard</p>
+            </div>
+
+            <div className="dash_content">
+                {content.map(data => (
+                    <div className='dash_contents'>
+                        <div className="dash_content_img"></div>
+
+                        <div className="info">
+                            <h1><u id="titleU">{data.title}</u></h1>
+                            <p id="adminAuth">{data.author}</p>
+                            <p id="adminDesc">{data.desc}</p>
+                            <a id="adminLink" href="#">{data.link}</a>
+                        </div>
+
+                    </div>
+                ))}
+
+
+
+            </div>
+
         </div>
     );
 }
--- src/components/UserDashboard.jsx
+++ src/components/UserDashboard.jsx
@@ -1,7 +1,36 @@
+import '../styles/UserDashboard.css'
+
 const UserDashboard = () => {
     return (
         <div className="user_dashboard">
-            <h1>Welcome to User Dashboard</h1>
+            <div className="img">
+                <p>Welcome to User Dashboard</p>
+            </div>
+
+            <div className="user_dash">
+                <div className="learning">
+                    <h1><u>Learning Lab</u></h1>
+                    <p>The mission of the Library’s Learning Lab is to create an environment in which Greenwich Library staff and patrons can empower themselves with a practical understanding of current technology.</p>
+                </div>
+
+                <div className='dashContent'>
+                    <div className="dashImg"></div>
+
+                    <div className="information">
+                        <p>LEARN</p>
+                        <h1><u>LinkedIn Learning</u></h1>
+                        <p>LinkedIn Learning (formerly Lynda) is an online educational site that features thousands of courses and video tutorials where you can discover, complete, and track courses related to your industry or interests.</p>
+                    </div>
+
+                </div>
+            </div>
+
+            <div className="footer">
+                <h1>Learn Something New or Get Tech Help</h1>
+                <p>Register on the online calendar. For more information call (203) 622-7920.</p>
+
+                <button>{"View Calendar ->"}</button>
+            </div>
         </div>
     );
 }
